b56cc3a88a02e4c4782f76cfeb0991b879f40637
LDJ <eowls5602@gmail.com>
2024-07-18 11:30:19 +0900

    섹션 컴퍼넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -13,26 +13,33 @@ import Channel from './pages/Channel'
 import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
 
 
 
 const App = () => {
     return (
       <BrowserRouter>
-        <Routes>
-            <Route path='/' element = {<Home/>} />
-            <Route path='/today' element = {<Today/>} />
-            <Route path='/developer' element = {<Developer/>} />
-            <Route path='/webd' element = {<Webd/>} />
-            <Route path='/website' element = {<Website/>} />
-            <Route path='/gsap' element = {<Gsap/>} />
-            <Route path='/port' element = {<Port/>} />
-            <Route path='/youtube' element = {<Youtube/>} />
-            <Route path='/channel/:channelID' element = {<Channel/>} />
-            <Route path='/video/:videoID' element = {<Video/>} />
-            <Route path='/search/:searchID' element = {<Search/>} />
-            <Route path='/*' element = {<Not/>} />
-        </Routes>
+        <Header />
+        <Main>
+          <Routes>
+              <Route path='/' element = {<Home/>} />
+              <Route path='/today' element = {<Today/>} />
+              <Route path='/developer' element = {<Developer/>} />
+              <Route path='/webd' element = {<Webd/>} />
+              <Route path='/website' element = {<Website/>} />
+              <Route path='/gsap' element = {<Gsap/>} />
+              <Route path='/port' element = {<Port/>} />
+              <Route path='/youtube' element = {<Youtube/>} />
+              <Route path='/channel/:channelID' element = {<Channel/>} />
+              <Route path='/video/:videoID' element = {<Video/>} />
+              <Route path='/search/:searchID' element = {<Search/>} />
+              <Route path='/*' element = {<Not/>} />
+          </Routes>
+        </Main>
+        <Footer />
       </BrowserRouter>
     )
 }
---
+++ src/components/section/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main id = 'main' role='main'>
+    Main
+    </main>
+  )
+}
+
+export default Main
