b8cd72ea72407594764925dcac0270c762da107e
daejin5602 <eowls5602@gmail.com>
2025-09-12 21:26:24 +0900

    섹션 컴퍼넌트 구조화하기

---
+++ src/assets/scss/section/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header id='header' role='banner'>
+        header
+    </header>
+  )
+}
+
+export default Header
---
+++ src/assets/scss/section/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main id='main' role='main'>
+        main
+    </main>
+  )
+}
+
+export default Main
